3dcd12875c8f6f2e9c9dddbe3c9b6ac57924c2a1
myamyan <amorimaffonsoyasmin@gmail.com>
2023-10-24 15:13:06 -0300

    2

--- api/src/Repository/pedidoRepository.js
+++ api/src/Repository/pedidoRepository.js
@@ -5,7 +5,7 @@ export async function CadastroPedido( pedido ) {
     const comando = `
 
     insert into tb_pedido ( id_cliente, id_produto, dt_datapedido, ds_metodopagamento, ds_entrega, ds_status_pedido )
-        values( ?, ?, ? );
+        values( ?, ?, ?, ?, ?, ?);
 
     `
 
