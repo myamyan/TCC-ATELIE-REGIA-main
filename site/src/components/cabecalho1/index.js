22f1d190517411ba393c29dc5d7c94c99b9e585a
myamyan <amorimaffonsoyasmin@gmail.com>
2023-10-25 18:24:53 -0300

    25102023

--- site/src/components/cabecalho1/index.js
+++ site/src/components/cabecalho1/index.js
@@ -1,41 +1,48 @@
 
+import Menu from '../menu';
 import './index.scss';
 
 export default function Cabecalho1() {
   return (
 
+    <div className="auau-cab1">
 
-    <div className="cabecalho1">
-
-      <div className='cab1-esquerda'>
+      <Menu />
 
 
-        <div>
-          <img src="/assets/images/zabugaaa.png" alt="" className="menu1"/>
+      <div className="cabecalho1">
+
+
+        <div className='cab1-esquerda'>
+
+
+          <div>
+            <img src="/assets/images/zabugaaa.png" alt="" className="menu1" />
+
+          </div>
+
+          <h2>ATELIÊ RÉGIA</h2>
+
+          <div>
+            <img src="/assets/images/logo.png" alt="" className="logo1" />
+
+          </div>
 
         </div>
 
-        <h2>ATELIÊ RÉGIA</h2>
 
-        <div>
-          <img src="/assets/images/logo.png" alt="" className="logo1" />
+        <div className='cab1-direita'>
+
+
+          <img className="img1-cab1" src="/assets/images/lupis.png" alt="" />
+
+          <h3><a href="/login/cliente">LOGIN</a></h3>
+
+          <img className="img2-cab1" src="/assets/images/redfivediamonds.png" alt="" />
 
         </div>
 
       </div>
-
-
-      <div className='cab1-direita'>
-
-
-        <img className="img1-cab1" src="/assets/images/lupis.png" alt="" />
-
-        <h3><a href="/login/cliente">LOGIN</a></h3>
-
-        <img className="img2-cab1" src="/assets/images/redfivediamonds.png" alt="" />
-
-      </div>
-
 
     </div>
   );
